f272658e5704dc13c7799bb685811f34eb6b47b5
shuhulnehru <shuhul.college@gmail.com>
2024-11-12 21:06:32 -0500

    Implementation of Ratings

--- routes/mentees.js
+++ routes/mentees.js
@@ -1,86 +1,86 @@
-import express from 'express';
-import { menteeData, mentorData } from '../data/index.js';
-import {dbConnection, closeConnection} from '../config/mongoConnection.js';
-import { ObjectId }  from 'mongodb';
-import { mentees } from '../config/mongoCollections.js';
+// import express from 'express';
+// import { menteeData, mentorData } from '../data/index.js';
+// import {dbConnection, closeConnection} from '../config/mongoConnection.js';
+// import { ObjectId }  from 'mongodb';
+// import { mentees } from '../config/mongoCollections.js';
 
-const router = express.Router();
+// const router = express.Router();
 
-router
-    .route('/mentees')
-    .get(
-        async (req, res) => {
-            try{
-              let mentees = await menteeData.getAllMentees();
-              return res.status(200).json(mentees);
-            }catch(e){
-              return res.status(500).json({e});
-            }
-        }
-    )
-    .post(async(req, res) => {
-        const newMentee = req.body;
+// router
+//     .route('/mentees')
+//     .get(
+//         async (req, res) => {
+//             try{
+//               let mentees = await menteeData.getAllMentees();
+//               return res.status(200).json(mentees);
+//             }catch(e){
+//               return res.status(500).json({e});
+//             }
+//         }
+//     )
+//     .post(async(req, res) => {
+//         const newMentee = req.body;
 
-        //Error Handling Will be done here
+//         //Error Handling Will be done here
 
-        try{
-            let menteeCreate = await menteeData.createMentee(newMentee.name, newMentee.email, newMentee.password_hash, newMentee.parent_email, newMentee.profile_image, newMentee.created_at);
+//         try{
+//             let menteeCreate = await menteeData.createMentee(newMentee.name, newMentee.email, newMentee.password_hash, newMentee.parent_email, newMentee.profile_image, newMentee.created_at);
 
-            return res.status(200).json(menteeCreate);
-        }catch(e){
-            return res.status(500).json({error: e});
-        }
-    }
-    );
+//             return res.status(200).json(menteeCreate);
+//         }catch(e){
+//             return res.status(500).json({error: e});
+//         }
+//     }
+//     );
 
-router
-    .route('/mentees/:menteeId')
-    .get(async (req, res) => {
-        try{
-            let menteeId = req.params.menteeId.trim();
+// router
+//     .route('/mentees/:menteeId')
+//     .get(async (req, res) => {
+//         try{
+//             let menteeId = req.params.menteeId.trim();
 
-            //Error Handling Will be done
+//             //Error Handling Will be done
 
-            let mentee = await menteeData.getMenteeById(menteeId);
-            return res.status(200).json(mentee);
+//             let mentee = await menteeData.getMenteeById(menteeId);
+//             return res.status(200).json(mentee);
 
-        }catch(e){
-            return res
-            .status(404)
-            .json({error: e});
-        }
-    }
-    )
-    .delete(async (req, res) => {
-        try{
-            let menteeId = req.params.menteeId.trim();
+//         }catch(e){
+//             return res
+//             .status(404)
+//             .json({error: e});
+//         }
+//     }
+//     )
+//     .delete(async (req, res) => {
+//         try{
+//             let menteeId = req.params.menteeId.trim();
 
-            //Error Handling Will be done
+//             //Error Handling Will be done
 
-            let mentee = await menteeData.removeMentee(menteeId);
-            return res.status(200).json({_id: menteeId, deleted: "true"});
+//             let mentee = await menteeData.removeMentee(menteeId);
+//             return res.status(200).json({_id: menteeId, deleted: "true"});
 
-        }catch(e){
-            return res
-            .status(404)
-            .json({error: e});
-        }
-    })
-    .put(async (req, res) =>{
-        try{
-            let menteeId = req.params.menteeId.trim();
-            //Error Handling Will be done here
+//         }catch(e){
+//             return res
+//             .status(404)
+//             .json({error: e});
+//         }
+//     })
+//     .put(async (req, res) =>{
+//         try{
+//             let menteeId = req.params.menteeId.trim();
+//             //Error Handling Will be done here
 
-            const updatedMentee = req.body;
+//             const updatedMentee = req.body;
 
-            // Error Handling Will be done here
+//             // Error Handling Will be done here
 
-            const mentee = await menteeData.createMentee(menteeId,updatedMentee.name, updatedMentee.email, updatedMentee.password_hash, updatedMentee.parent_email, updatedMentee.profile_image, updatedMentee.created_at);
+//             const mentee = await menteeData.createMentee(menteeId,updatedMentee.name, updatedMentee.email, updatedMentee.password_hash, updatedMentee.parent_email, updatedMentee.profile_image, updatedMentee.created_at);
 
-            return res.status(200).json(menteeCreate);
-        }catch(e){
-            return res.status(500).json({error: e});
-        }
-    }
-    );
+//             return res.status(200).json(menteeCreate);
+//         }catch(e){
+//             return res.status(500).json({error: e});
+//         }
+//     }
+//     );
     
--- routes/mentors.js
+++ routes/mentors.js
@@ -1,86 +1,86 @@
-import express from 'express';
-import { menteeData, mentorData } from '../data/index.js';
-import {dbConnection, closeConnection} from '../config/mongoConnection.js';
-import { ObjectId }  from 'mongodb';
-import { mentees } from '../config/mongoCollections.js';
+// import express from 'express';
+// import { menteeData, mentorData } from '../data/index.js';
+// import {dbConnection, closeConnection} from '../config/mongoConnection.js';
+// import { ObjectId }  from 'mongodb';
+// import { mentees } from '../config/mongoCollections.js';
 
-const router = express.Router();
+// const router = express.Router();
 
-router
-    .route('/mentors')
-    .get(
-        async (req, res) => {
-            try{
-              let mentors = await mentorData.getAllMentors();
-              return res.status(200).json(mentors);
-            }catch(e){
-              return res.status(500).json({e});
-            }
-        }
-    )
-    .post(async(req, res) => {
-        const newMentor = req.body;
+// router
+//     .route('/mentors')
+//     .get(
+//         async (req, res) => {
+//             try{
+//               let mentors = await mentorData.getAllMentors();
+//               return res.status(200).json(mentors);
+//             }catch(e){
+//               return res.status(500).json({e});
+//             }
+//         }
+//     )
+//     .post(async(req, res) => {
+//         const newMentor = req.body;
 
-        //Error Handling Will be done here
+//         //Error Handling Will be done here
 
-        try{
-            let mentorCreate = await menteeData.createMentee(newMentor.name, newMentor.email, newMentor.password_hash, newMentor.profile_image, newMentor.created_at, newMentor.qualifications, newMentor.experience, newMentor.availability, newMentor.approval_status);
+//         try{
+//             let mentorCreate = await menteeData.createMentee(newMentor.name, newMentor.email, newMentor.password_hash, newMentor.profile_image, newMentor.created_at, newMentor.qualifications, newMentor.experience, newMentor.availability, newMentor.approval_status);
 
-            return res.status(200).json(mentorCreate);
-        }catch(e){
-            return res.status(500).json({error: e});
-        }
-    }
-    );
+//             return res.status(200).json(mentorCreate);
+//         }catch(e){
+//             return res.status(500).json({error: e});
+//         }
+//     }
+//     );
 
-router
-    .route('/mentors/:mentorId')
-    .get(async (req, res) => {
-        try{
-            let mentorId = req.params.mentorId.trim();
+// router
+//     .route('/mentors/:mentorId')
+//     .get(async (req, res) => {
+//         try{
+//             let mentorId = req.params.mentorId.trim();
 
-            //Error Handling Will be done
+//             //Error Handling Will be done
 
-            let mentor = await mentorData.getMentorById(mentorId);
-            return res.status(200).json(mentor);
+//             let mentor = await mentorData.getMentorById(mentorId);
+//             return res.status(200).json(mentor);
 
-        }catch(e){
-            return res
-            .status(404)
-            .json({error: e});
-        }
-    }
-    )
-    .delete(async (req, res) => {
-        try{
-            let mentorId = req.params.v.trim();
+//         }catch(e){
+//             return res
+//             .status(404)
+//             .json({error: e});
+//         }
+//     }
+//     )
+//     .delete(async (req, res) => {
+//         try{
+//             let mentorId = req.params.v.trim();
 
-            //Error Handling Will be done
+//             //Error Handling Will be done
 
-            let mentor = await mentorData.removeMentor(mentorId);
-            return res.status(200).json({_id: mentorId, deleted: "true"});
+//             let mentor = await mentorData.removeMentor(mentorId);
+//             return res.status(200).json({_id: mentorId, deleted: "true"});
 
-        }catch(e){
-            return res
-            .status(404)
-            .json({error: e});
-        }
-    })
-    .put(async (req, res) =>{
-        try{
-            let mentorId = req.params.mentorId.trim();
-            //Error Handling Will be done here
+//         }catch(e){
+//             return res
+//             .status(404)
+//             .json({error: e});
+//         }
+//     })
+//     .put(async (req, res) =>{
+//         try{
+//             let mentorId = req.params.mentorId.trim();
+//             //Error Handling Will be done here
 
-            const updatedMentor = req.body;
+//             const updatedMentor = req.body;
 
-            // Error Handling Will be done here
+//             // Error Handling Will be done here
 
-            let mentorCreate = await menteeData.createMentee(updatedMentor.name, updatedMentor.email, updatedMentor.password_hash, updatedMentor.profile_image, updatedMentor.created_at, updatedMentor.qualifications, updatedMentor.experience, updatedMentor.availability, updatedMentor.approval_status);
+//             let mentorCreate = await menteeData.createMentee(updatedMentor.name, updatedMentor.email, updatedMentor.password_hash, updatedMentor.profile_image, updatedMentor.created_at, updatedMentor.qualifications, updatedMentor.experience, updatedMentor.availability, updatedMentor.approval_status);
 
-            return res.status(200).json(mentorCreate);
-        }catch(e){
-            return res.status(500).json({error: e});
-        }
-    }
-    );
+//             return res.status(200).json(mentorCreate);
+//         }catch(e){
+//             return res.status(500).json({error: e});
+//         }
+//     }
+//     );
     
