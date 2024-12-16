d63fc50f13fe0307be7cbb8fd578e6543628546e
Rohan Deshmukh <deshmukhr849@gmail.com>
2024-12-16 17:17:58 -0500

    page render on 404

--- routes/index.js
+++ routes/index.js
@@ -5,25 +5,27 @@ import { parentEmailRoutes } from "./parent.js";
 import { postRoutes } from "./posts.js";
 import { ratingsRoutes } from "./rating.js";
 import { rootRoutes } from "./root.js";
-import { sessionRoutes } from './sessions.js';
+import { sessionRoutes } from "./sessions.js";
 import { subjectRoutes } from "./subject_areas.js";
-
-
 
 const constructorMethod = (app) => {
     app.use("/", rootRoutes);
     app.use("/mentee", menteeRoutes);
     app.use("/mentor", mentorRoutes);
     app.use("/ratings", ratingsRoutes);
-    app.use('/sessions', sessionRoutes);
-    app.use('/subjects', subjectRoutes);
-    app.use('/parentEmailNotify',parentEmailRoutes);
-    app.use('/forum', postRoutes);
-    app.use('/admin', adminRoutes);
-
+    app.use("/sessions", sessionRoutes);
+    app.use("/subjects", subjectRoutes);
+    app.use("/parentEmailNotify", parentEmailRoutes);
+    app.use("/forum", postRoutes);
+    app.use("/admin", adminRoutes);
 
     app.use("*", (req, res) => {
-        res.status(404).json({ error: "Not found" });
+        return res.status(404).render("error", {
+            pageTitle: "Page Not Found!",
+            headerOptions: req.headerOptions,
+            errorMessage: "This page does not exist.",
+            errorTitle: "404 - Not found",
+        });
     });
 };
 
